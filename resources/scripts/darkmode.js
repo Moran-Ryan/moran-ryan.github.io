e376c12a51d9df7b294c018237701ff1ed6fffe3
Ryan Moran <ryan@dittomail.com>
2025-02-10 15:20:03 -0500

    Clean up and improve functionality of darkmode.js, alter darkmode css styling

--- resources/scripts/darkmode.js
+++ resources/scripts/darkmode.js
@@ -1,11 +1,3 @@
-// Check if darkOn exists in localStorage, and create it if it doesn't
-if (!localStorage.getItem("darkOn")) {
-    console.log("darkOn item not found in localStorage, creating");
-    localStorage.setItem("darkOn", "false");
-} else {
-    console.log(`darkOn item found in localStorage. Its value is ${localStorage.getItem("darkOn")}`)
-}
-
 // Variable to acess state of the theme toggle checkbox
 const darkToggle = document.getElementById("darkmode");
 // Variable to access the stylesheet link for the theme
@@ -22,18 +14,28 @@ const makeDark = () => {
     theme.setAttribute('href', "./resources/css/dark.css");
 }
 
+// Check if darkOn exists in localStorage, apply appropriate setting if it does, and create it if it doesn't
+if (!localStorage.getItem("darkOn")) {
+    console.log("darkOn item not found in localStorage, creating");
+    localStorage.setItem("darkOn", "false");
+} else if (localStorage.getItem("darkOn") === "true") {
+    makeDark();
+    console.log(`darkOn item found in localStorage. Its value is ${localStorage.getItem("darkOn")}`)
+} else {
+    console.log(`darkOn item found in localStorage. Its value is ${localStorage.getItem("darkOn")}`)
+}
+
+
 // Calls makeDark() or makeLight() functions based on value of darkOn in localStorage
 const toggleColors = () => {
     if (localStorage.getItem("darkOn") === "true") {
         console.log("Tuning dark mode off");
         localStorage.setItem("darkOn", "false");
         makeLight();
-        console.log(localStorage.getItem("darkOn"));
     } else if (localStorage.getItem("darkOn") === "false") {
         console.log("Tuning dark mode on");
         localStorage.setItem("darkOn", "true");
         makeDark();
-        console.log(localStorage.getItem("darkOn"));
     } else {
         console.log("Something weird is going on");
     }
